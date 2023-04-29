8ad6197d1a6b725ee7bbd6eabc0fb90e645e141b
iNfidelAim <110556919+iNfidelAim@users.noreply.github.com>
2023-04-29 14:36:21 +0300

    Add files via upload

---
+++ src/main/java/com/afanasiev/sheets/AnalogGoogleSheetsApplication.java
@@ -0,0 +1,13 @@
+package com.afanasiev.sheets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnalogGoogleSheetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnalogGoogleSheetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/afanasiev/sheets/AnalogGoogleSheetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.afanasiev.sheets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalogGoogleSheetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
